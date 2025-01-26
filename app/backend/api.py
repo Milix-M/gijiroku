833c6f0b7e2739a0539709a793e9a80e00700527
Milix-M <vta.kokitsuji@gmail.com>
2025-01-26 22:36:02 +0900

    プロンプト忘れてたので追加

--- app/backend/api.py
+++ app/backend/api.py
@@ -6,6 +6,15 @@ from util.converter import convert_mp4_to_mp3
 
 app = Flask(__name__, instance_relative_config=True)
 
+prompt = """次の文章を議事録にしてください。
+議事録は各話題ごとに会話形式でまとめて下さい。議事録はマークダウン記法で作成してください。
+この文章を生成したのは文字起こしAIなので、誤った文字だと思われる場合には正しいと思える文章に修正して下さい。
+同じ人が話したと思われる文章は文章の先頭に[SPEAKER n]とつけてまとめて下さい。nには0からカウントアップした話者IDが入ります。
+話者ごとにIDは割り当てられます。話者の名前が分かる場合はその人の名前を入れて[話者の名前]としてください。
+この会議で決まった決定事項がある場合はそれもまとめて下さい。決定事項は誰が、いつまでに何をしなければいかないかをまとめて下さい。
+
+以下は議事録を文字起こしした文章です。
+"""
 
 @app.route("/generate-minutes", methods=["POST"])
 def generate_minutes():
@@ -28,7 +37,7 @@ def generate_minutes():
     print(meeting_text)
 
     # 議事録を生成
-    minutes = gemma.talk_llm(meeting_text)
+    minutes = gemma.talk_llm(minutes=meeting_text, prompt=prompt)
     print(minutes)
 
     return {"result": True, "minutes": minutes}
